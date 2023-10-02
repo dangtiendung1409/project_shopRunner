842610041cd634fa4ce48b4d1025ff78a12e5e48
chipchippp <locnvth2209036@fpt.edu.vn>
2023-10-02 07:01:39 +0700

    database

---
+++ app/Models/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $table = "products";
+
+    protected $primaryKey = "id";
+
+    protected $fillable = [
+        "name",
+        "slug",
+        "color",
+        "size",
+        "price",
+        "thumbnail",
+        "qty",
+        "status",
+        "description",
+        "category_id"
+    ];
+}
