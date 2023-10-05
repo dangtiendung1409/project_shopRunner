d9988cb5bf42f33a1f1f78b4382ed899f85579d2
chipchippp <locnvth2209036@fpt.edu.vn>
2023-10-05 15:25:37 +0700

    Update icon.blade.php

--- resources/views/layouts/customer/icon.blade.php
+++ resources/views/layouts/customer/icon.blade.php
@@ -1,27 +1,58 @@
-<div class="arcontactus-widget">
-<div class="hotline-phone-ring-wrap1">
-    <div class="hotline-phone-ring">
-        <div class="hotline-phone-ring-circle1"></div>
-        <div class="hotline-phone-ring-circle-fill1"></div>
-        <div class="hotline-phone-ring-img-circle1">
-            <a href="tel:0903177877" class="pps-btn-img">
-                <img src="/customer/img/icons8-zalo-48.png" alt="Gọi điện thoại" width="100px">
-            </a>
-        </div>
-    </div>
+{{--<div class="arcontactus-widget">--}}
+{{--<div class="hotline-phone-ring-wrap1">--}}
+{{--    <div class="hotline-phone-ring">--}}
+{{--        <div class="hotline-phone-ring-circle1"></div>--}}
+{{--        <div class="hotline-phone-ring-circle-fill1"></div>--}}
+{{--        <div class="hotline-phone-ring-img-circle1">--}}
+{{--            <a href="tel:0903177877" class="pps-btn-img">--}}
+{{--                <img src="/customer/img/icons8-zalo-48.png" alt="Gọi điện thoại" width="100px">--}}
+{{--            </a>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--    <div class="hotline-phone-ring-wrap2">--}}
+{{--        <div class="hotline-phone-ring">--}}
+{{--            <div class="hotline-phone-ring-circle2"></div>--}}
+{{--            <div class="hotline-phone-ring-circle-fill2"></div>--}}
+{{--            <div class="hotline-phone-ring-img-circle2">--}}
+{{--                <a href="tel:0903177877" class="pps-btn-img">--}}
+{{--                    <img src="/customer/img/messenger.png" alt="Gọi điện thoại" width="50px">--}}
+{{--                </a>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
+{{--</div>--}}
+{{--<a href="#" id="backTop">--}}
+{{--    <i class="fa-solid fa-chevron-up fa-bounce"></i>--}}
+{{--</a>--}}
+
+<!-- Messenger Plugin chat Code -->
+<div id="fb-root"></div>
+
+<!-- Your Plugin chat code -->
+<div id="fb-customer-chat" class="fb-customerchat">
 </div>
-    <div class="hotline-phone-ring-wrap2">
-        <div class="hotline-phone-ring">
-            <div class="hotline-phone-ring-circle2"></div>
-            <div class="hotline-phone-ring-circle-fill2"></div>
-            <div class="hotline-phone-ring-img-circle2">
-                <a href="tel:0903177877" class="pps-btn-img">
-                    <img src="/customer/img/messenger.png" alt="Gọi điện thoại" width="50px">
-                </a>
-            </div>
-        </div>
-    </div>
-</div>
-<a href="#" id="backTop">
-    <i class="fa-solid fa-chevron-up fa-bounce"></i>
-</a>
+
+<script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "146730891851541");
+    chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
+<!-- Your SDK code -->
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v18.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
