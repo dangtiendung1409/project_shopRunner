d5677503bf4bd4f997dcc4cd67dcb9004cb04687
chipchippp <locnvth2209036@fpt.edu.vn>
2023-10-05 16:16:19 +0700

    database

--- resources/views/pages/customer/categoryShop.blade.php
+++ resources/views/pages/customer/categoryShop.blade.php
@@ -27,7 +27,7 @@
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-sm-6">
                                 <div class="shop__product__option__left">
-                                    <p>Showing 1–12 of 126 results</p>
+                                    <p>{{$products->total()}}</p>
                                 </div>
                             </div>
                         </div>
@@ -143,17 +143,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="product__pagination">
-                                <a class="active" href="#">1</a>
-                                <a href="#">2</a>
-                                <a href="#">3</a>
-                                <span>...</span>
-                                <a href="#">21</a>
-                            </div>
-                        </div>
-                    </div>
+                    {!! $products->links("pagination::bootstrap-4") !!}
                 </div>
             </div>
         </div>
