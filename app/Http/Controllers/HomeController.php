5b5d8a1cff523cf2955e81b45b4aa10fc2dcc455
Minh12s <minhtnth2209037@fpt.edu.vn>
2023-10-13 14:22:22 +0700

    add to cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -77,8 +77,62 @@ class HomeController
         return view("pages.customer.shopDetails", compact("product", "variants", "relate","colorAvailability","sizeAvailability"));
     }
 
-    public function cartShop(){
-        return view("pages.customer.cartShop");
+    public function addToCart(Product $product, Request $request){
+        $buy_qty = $request->get("buy_qty");
+        $cartShop = session()->has("cartShop")?session("cartShop"):[];
+        foreach ($cartShop as $item){
+            if($item->id == $product->id){
+                $item->buy_qty = $item->buy_qty + $buy_qty;
+                session(["cartShop"=>$cartShop]);
+                return redirect()->back()->with("success","Đã thêm sản phẩm vào giỏ hàng");
+            }
+        }
+        $product->buy_qty = $buy_qty;
+        $cartShop[] = $product;
+        session(["cartShop"=>$cartShop]);
+        return redirect()->back()->with("success","Đã thêm sản phẩm vào giỏ hàng");
+    }
+    public function cartShop()
+    {
+        $cartShop = session()->has("cartShop")?session("cartShop"):[];
+        $subtotal = 0;
+        $can_checkout = true;
+        foreach ($cartShop as $item){
+            $subtotal += $item->price * $item->buy_qty;
+            if($item->buy_qty > $item->qty)
+                $can_checkout = false;
+        }
+        $total = $subtotal*1.1; // vat: 10%
+        return view('pages.customer.cartShop', compact('cartShop', 'subtotal', 'total', 'can_checkout'));
+    }
+    public function deleteFromCart(Product $product){
+        $cartShop = session()->has("cartShop") ? session("cartShop") : [];
+        $cartShop = array_filter($cartShop, function ($item) use ($product) {
+            return $item->id != $product->id;
+        });
+        session()->put("cartShop", $cartShop);
+        return redirect()->back()->with("success", "Đã xóa sản phẩm khỏi giỏ hàng");
+
+
+    }
+    public function updateCart(Product $product, Request $request)
+    {
+        $buy_qty = $request->get("buy_qty");
+        $cartShop = session()->has("cartShop") ? session("cartShop") : [];
+
+        foreach ($cartShop as $item) {
+            if ($item->id == $product->id) {
+                $item->buy_qty = $buy_qty;
+                break;
+            }
+        }
+
+        session(["cartShop" => $cartShop]);
+        return redirect()->back()->with("success", "Đã cập nhật giỏ hàng");
+    }
+    public function clearCart(){
+        session()->forget("cartShop");
+        return redirect()->back()->with("success", "Đã xóa tất cả sản phẩm khỏi giỏ hàng");
     }
 
     public function contactShop(){
