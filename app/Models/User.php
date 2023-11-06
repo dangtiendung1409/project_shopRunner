696f79d77aafe062e1dcbe97e900b0edd94e5979
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2023-11-06 12:52:15 +0700

    update lại hasFavorite ở User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -28,6 +29,14 @@ class User extends Authenticatable
 
 
     ];
+
+    public function hasFavorite($productName)
+    {
+        return DB::table('favorite_orders')
+            ->where('user_id', $this->id)
+            ->where('name', $productName)
+            ->exists();
+    }
 
     /**
      * The attributes that should be hidden for serialization.
