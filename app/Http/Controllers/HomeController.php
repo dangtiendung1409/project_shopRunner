05ee46312d06ac98ce7b70750ecb315acebd46a8
quanglong29074 <114908074+quanglong29074@users.noreply.github.com>
2023-11-09 14:52:39 +0700

    update profile

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 use Srmklive\PayPal\Services\PayPal as PayPalClient;
 
 class HomeController
@@ -414,6 +415,45 @@ class HomeController
     public function EditProfile(){
         return view("pages.customer.edit-profile");
     }
+    public function updateProfile (Request $request)
+    {
+        // Validate the form input
+        $request->validate([
+            "name"=>"required|min:6",
+            "address"=>"required",
+            "tel"=> "required|min:9|max:11",
+            "email"=>"required",
+            //   'thumbnail' => 'image|mimes:jpeg,png,jpg,gif|max:2048', // Adjust file types and size limit as needed
+        ]);
+
+        try {
+            $user = Auth::user();
+
+            $thumbnail = $user->thumbnail;
+
+            // Handle file upload
+            if ($request->hasFile("thumbnail")) {
+                $path = public_path("images");
+                $file = $request->file("thumbnail");
+                $file_name = Str::random(5) . time() . Str::random(5) . "." . $file->getClientOriginalExtension();
+                $file->move($path, $file_name);
+                $thumbnail = "/images/" . $file_name;
+            }
+
+            $user->update([
+                "name" => $request->input("name"),
+                "email" => $request->input("email"),
+                "tel" => $request->input("tel"),
+                "address" => $request->input("address"),
+                "thumbnail" => $thumbnail,
+            ]);
+
+            return redirect()->to('/profile'); // Redirect to the profile page after successful update
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage()); // Pass the error message to the view
+        }
+    }
+
     public function ThankYou(Order $order){
 //        dd(session("cartShop"));
         return view("pages.customer.thankYou",compact("order"));
