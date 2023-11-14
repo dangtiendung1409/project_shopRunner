339ba34f4028d30530ef5c1ce9e87d95ec47fe1c
chipchippp <locnvth2209036@fpt.edu.vn>
2023-11-14 10:30:10 +0700

    sidebar admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -1,9 +1,9 @@
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
-    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="/images/hay.jpg" width="50px"
-                                        alt="User Image">
+    <div class="app-sidebar__user">
+        <img class="app-sidebar__user-avatar" src="/images/hay.jpg" width="50px" alt="User Image">
         <div>
-            <p class="app-sidebar__user-name"><b>Võ Trường</b></p>
+            <p class="app-sidebar__user-name"><b>{{auth()->user()->name}}</b></p>
             <p class="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
         </div>
     </div>
